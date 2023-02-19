53a0e57d988dbcd2075d675dc4c8a6936644685d
Rahul Chauhan <r.rahul.c@gmail.com>
2023-02-19 12:43:36 +0530

    update

--- contracts/MultiSigWallet.sol
+++ contracts/MultiSigWallet.sol
@@ -1,14 +1,18 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
-
+/// @title Multi-Signature Wallet
+/// @author Rahul Chauhan
+/// @notice This contract implements a multi-signatue wallet.
 contract MultiSigWallet {
+  /// @notice variable declarations to store owner addresses
   address[] public owners;
   mapping(address => bool) ownerExists;
+
+  /// @notice variable declarations to store number of confirmations required to transact
   uint256 public numberOfConfirmationsRequired;
 
+  /// @notice Transaction Structure
   struct transaction {
     address to;
     uint256 value;
@@ -17,10 +21,15 @@ contract MultiSigWallet {
     uint256 confirmations;
   }
 
+  /// @notice Array of transactions and a mapping to check if transaction is confirmed.
   transaction[] public transactions;
-
   mapping(uint256 => mapping(address => bool)) public isConfirmed;
 
+  /// Event emitted when transaction is submitted.
+  /// @param _transactionIndex (transaction index)
+  /// @param _address  (address submitting the transaction)
+  /// @param _value (value associated with transaction)
+  /// @param _data (data associated with transaction)
   event transactionSubmitted(
     uint256 _transactionIndex,
     address _address,
@@ -28,12 +37,23 @@ contract MultiSigWallet {
     bytes _data
   );
 
+  /// Event emitted when transaction is approved.
+  /// @param _transactionIndex (transaction index)
+  /// @param _address (address submitting the transaction)
   event transactionApproved(uint256 _transactionIndex, address _address);
 
+  /// Event emitted when transaction is withdrawn
+  /// @param _transactionIndex (transaction index)
+  /// @param _address (address submitting the transaction)
   event approvalWithdrawn(uint256 _transactionIndex, address _address);
 
+  /// Event emitted when transaction is executed
+  /// @param _transactionIndex (transaction index)
   event transactionExecuted(uint256 _transactionIndex);
 
+  /// Modifier to check if number of confirmations set at the time of deployment are valid
+  /// @param _owners (array of owners)
+  /// @param _confirmation (number of confirmations)
   modifier numberOfConfirmationsValid(
     address[] memory _owners,
     uint256 _confirmation
@@ -42,16 +62,21 @@ contract MultiSigWallet {
     _;
   }
 
+  /// Modifier to check if message sender is among owners
   modifier isOwner() {
     require(ownerExists[msg.sender] == true, "Not Authorized");
     _;
   }
 
+  /// Modifier to check if transaction is present
+  /// @param _transactionIndex (transaction index)
   modifier transactionPresent(uint256 _transactionIndex) {
     require(_transactionIndex <= transactions.length, "Transaction not found");
     _;
   }
 
+  /// Modifier to check if transaction is confirmed (approved by all owners)
+  /// @param _transactionIndex (transaction index)
   modifier transactionConfirmed(uint256 _transactionIndex) {
     require(
       transactions[_transactionIndex].confirmations >=
@@ -61,6 +86,8 @@ contract MultiSigWallet {
     _;
   }
 
+  /// Modifier to check that transaction is not executed.
+  /// @param  _transactionIndex (transaction index)
   modifier transactionNotExecuted(uint256 _transactionIndex) {
     require(
       transactions[_transactionIndex].executed == false,
@@ -69,6 +96,9 @@ contract MultiSigWallet {
     _;
   }
 
+  /// Modifier to check that transaction is not already approved by the message sender.
+  /// @param  _transactionIndex (transaction index)
+  /// @param  _from (message sender)
   modifier transactionNotAlreadyApprovedByAddress(
     uint256 _transactionIndex,
     address _from
@@ -80,6 +110,9 @@ contract MultiSigWallet {
     _;
   }
 
+  /// Modifier to check that transaction is already approved by the message sender.
+  /// @param  _transactionIndex (transaction index)
+  /// @param  _from (message sender)
   modifier transactionAlreadyApprovedByAddress(
     uint256 _transactionIndex,
     address _from
@@ -91,6 +124,9 @@ contract MultiSigWallet {
     _;
   }
 
+  /// Constructor
+  /// @param _owners  (array of owners of multi-sig wallet)
+  /// @param _numberOfConfirmationsRequired (number of confirmations required for transaction to be executed)
   constructor(
     address[] memory _owners,
     uint256 _numberOfConfirmationsRequired
@@ -103,6 +139,10 @@ contract MultiSigWallet {
     numberOfConfirmationsRequired = _numberOfConfirmationsRequired;
   }
 
+  /// Function to submit a transaction
+  /// @param _to (address of contract)
+  /// @param _value (value associated with transaction)
+  /// @param _data (data associated with transaction)
   function submitTransaction(
     address _to,
     uint256 _value,
@@ -113,6 +153,8 @@ contract MultiSigWallet {
     emit transactionSubmitted(transactionIndex, _to, _value, _data);
   }
 
+  /// Function to approve transaction
+  /// @param _transactionIndex (transactionIndex)
   function approveTransaction(
     uint256 _transactionIndex
   )
@@ -127,6 +169,8 @@ contract MultiSigWallet {
     emit transactionApproved(_transactionIndex, msg.sender);
   }
 
+  /// Function to withdraw transaction approval
+  /// @param _transactionIndex (transactionIndex)
   function withdrawApproval(
     uint256 _transactionIndex
   )
@@ -139,6 +183,8 @@ contract MultiSigWallet {
     emit approvalWithdrawn(_transactionIndex, msg.sender);
   }
 
+  /// Function to execute transaction approval
+  /// @param _transactionIndex (transactionIndex)
   function executeTransaction(
     uint256 _transactionIndex
   )
@@ -157,6 +203,8 @@ contract MultiSigWallet {
     emit transactionExecuted(_transactionIndex);
   }
 
+  /// Function to revoke approval
+  /// @param _transactionIndex (transactionIndex)
   function revokeApproval(
     uint256 _transactionIndex
   )
