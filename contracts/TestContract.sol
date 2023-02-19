72a29ae96b2478221f2a300b472da269051ff42e
Rahul Chauhan <r.rahul.c@gmail.com>
2023-02-19 12:51:09 +0530

    test contract

--- contracts/TestContract.sol
+++ contracts/TestContract.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
 
+/// @title Test Contract
+/// @author Rahul Chauhan
+/// @notice This is a test contract created for Multi-Sig Wallet to call its function incrementCounter as a transaction
 contract TestContract {
   uint256 public counter;
 
